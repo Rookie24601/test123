426cff2b9d6ba85877f23ad69be2540ff2e8bbaa
Rookie24601 <wararms13@gmail.com>
2021-12-07 16:29:16 +0000

    upd 1029

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 
     #path('', include(router.urls)),
     #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    path("admin/", admin.site.urls),
+    #path("admin/", admin.site.urls),
     path("", include("pages.urls")),
 ]
 
