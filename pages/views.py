dbb858324cd8db0b185fbc3054bdcb6b1c5644d5
Rookie24601 <wararms13@gmail.com>
2021-12-07 18:02:05 +0000

    lst update

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def homePageView(request):
     message = "before default"
     message = "default message"
-    print(request.GET)
+    req = request.__dict__
+    print("Raw http request: ",req)
+    print("http method: ", req['environ']['REQUEST_METHOD'])
+    print("http query: ", req['environ']['QUERY_STRING'])   
     return HttpResponse("Test message 123")
